cbae5145a979560dc17a674c5e70bedc19bc2a1b
Katythecat <kk.wilasinee@gmail.com>
2023-03-28 23:08:03 -0700

    solve the issue

--- src/Dog.java
+++ src/Dog.java
@@ -1,8 +1,8 @@
 public class Dog {
     public static void main(String[] args) {
         Pet pet=new Pet();
-        pet.type=dog;
+        pet.type="dog";
         pet.age="15";
-        pet.name=Pepo;
+        pet.name="Pepo";
     }
 }
